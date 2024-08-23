c2df1163bd8f2333422c6a34a88f76b2393bb42a
Zeitungsleser <ju.lg@gmx.de>
2024-08-23 22:12:51 +0200

    new selection modell

--- launcher.py
+++ launcher.py
@@ -7,8 +7,10 @@ import math
 
 games=[
     ["Pong", "python", r"/home/pi/Desktop/TurntableGames/DJPong/midi_test.py"],
-    ["Sokoban","python", r"/home/pi/Desktop/TurntableGames/dj-sokoban/sokoban.py"]
+    ["Sokoban","python", r"/home/pi/Desktop/TurntableGames/dj-sokoban/sokoban.py"],
 ]
+
+texte=[]
 
 sc = turtle.Screen()
 sc.bgcolor("black")
@@ -16,16 +18,6 @@ screenTk = sc.getcanvas().winfo_toplevel()
 screenTk.attributes("-fullscreen", True)
 sc.setup(width=1.0, height=1.0)
 
-#sketch = turtle.Turtle()
-#sketch.speed(0)
-#sketch.color("white")
-#sketch.penup()
-#sketch.hideturtle()
-#sketch.write(
-#    "  Pong        Tetris      Sokoban",
-#    align="center",
-#    font=("Terminal", 24, "normal"),
-#)
 num = len(games)
 for pos,game in enumerate(games):
     sketch = turtle.Turtle()
@@ -35,6 +27,9 @@ for pos,game in enumerate(games):
     sketch.hideturtle()
     sketch.goto(int(300 * math.cos(pos*2*math.pi/num)), int(300 * math.sin(pos*2*math.pi/num)))
     sketch.write(game[0], font=("Terminal",24,"normal"))
+    texte.append(sketch)
+
+game_angle = 360/num
 
 sel_ball = turtle.Turtle()
 sel_ball.speed(0)
@@ -46,7 +41,12 @@ sel_ball.goto(0,200)
 pyg = mido.Backend('mido.backends.pygame')
 inport = pyg.open_input('Xponent MIDI 1')
 
+#paramter of the ball, amount that it moved
 t=0
+#counter for the time till the game is selcted
+select_time = 0
+#id of the selected game
+select = None
 
 while True:
     choice=None
@@ -56,41 +56,30 @@ while True:
         elif msg.type == "note_on":
             if msg.note == 18:
                 exit()
-            elif msg.note >= 19 and msg.note <= 21:
-                t_tilde=t+360/(2*num)
-                choice = int(t_tilde // (360/num))
+            #elif msg.note >= 19 and msg.note <= 21:
+            #    t_tilde=t+360/(2*num)
+            #    choice = int(t_tilde // (360/num))
+    if t%game_angle <= 10 or t%game_angle >= game_angle-10:
+        select_new = None
+        if t%game_angle <= 10:
+            select_new = t//game_angle
+        elif t%game_angle >= game_angle-10:
+            select_new = t//game_angle +1
+            if select_new == num:
+                select_new = 0
+        if select == select_new:
+            select_time += 1
+            color_red = 0xff - select_time*0xff/100
+            color_green = 0x00 + select_time*0xff/100
+            texte[select].color(color_red,color_green,0)
+        select = select_new
+        if select_time == 100:
+            choice = select      
     sel_ball.goto(200*math.cos(t*2*math.pi/360),200*math.sin(t*2*math.pi/360))
     if t< - 360/(2*num):
         t += 360
     if t> 360-360/(2*num):
         t -= 360
-        
-#    system("clear")
-#    print(
-#        """
-#Programm wählen:
-#1) Pong
-#2) Tetris (noch nicht funktionsfähig)
-#3) Sokoban
-#"""
-#    )
-#    choice = input("> ").strip()
-#
-#    print(choice)
-#
     if choice!=None:
         print(choice)
         call([games[choice][1],games[choice][2]])
-#    choice=str(choice)
-#    if choice == "1":
-#        print("Starte Pong...")
-#        call(["python", r"/home/pi/Desktop/TurntableGames/DJPong/midi_test.py"])
-#    """if choice == "2":
-#        print("Starte Tetris...")
-#        call(["python", r"dj-tetris/__.py"])"""
-#    if choice == "3":
-#        print("Starte Sokoban...")
-#        call(
-#            ["python", r"/home/pi/Desktop/TurntableGames/dj-sokoban/sokoban.py"],
-#            stdin=open(r"/home/pi/Desktop/TurntableGames/dj-sokoban/boards.txt", "r"),
-#        )
